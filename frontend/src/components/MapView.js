8e88efbbf9de722929d37b1b0cd4896b2bbb01c2
AmosRB <96162009+AmosRB@users.noreply.github.com>
2025-05-04 07:22:37 +0300

    Update MapView.js תיקון ניהול קורדינאטות

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline, useMapEvents,  } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 
 function ClickHandler({ onMapClick }) {
@@ -10,6 +10,7 @@ function ClickHandler({ onMapClick }) {
   });
   return null;
 }
+
 function createEmojiIcon(emoji, label = '') {
   return L.divIcon({
     html: `<div style="font-size: 24px;">${emoji}</div><div style="font-size:10px;">${label}</div>`,
@@ -19,100 +20,84 @@ function createEmojiIcon(emoji, label = '') {
   });
 }
 
-
 export default function MapView({ center, landings, aliens, takilas, fighters, explosions, shots, onMapClick }) {
-  const getPosition = (item) =>
-    item.route && item.route.length > 0 && item.positionIdx !== undefined
-      ? [item.route[item.positionIdx][0], item.route[item.positionIdx][1]]
-      : [item.lat, item.lng];
-
   return (
-    <>
-      <MapContainer
-  center={center}
-  zoom={12}
-  style={{
-    height: '100vh',
-    width: '100vw',  // במקום 100%
-    margin: '0',
-    padding: '0',
-    position: 'absolute',
-    top: '0',
-    left: '0'
-  }}
->
+    <MapContainer
+      center={center}
+      zoom={12}
+      style={{
+        height: '100vh',
+        width: '100vw',
+        margin: '0',
+        padding: '0',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+      }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
+      <ClickHandler onMapClick={onMapClick} />
 
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
-        <ClickHandler onMapClick={onMapClick} />
+      {/* 🛸 נחיתות */}
+      {landings.map(l => (
+        <Marker key={`landing-${l.id}`} position={[l.lat, l.lng]} icon={createEmojiIcon('🛸', l.landingCode)}>
+          <Popup>{l.locationName}</Popup>
+        </Marker>
+      ))}
 
-        {/* 🛸 נחיתות */}
-        {landings.map(l => (
-          <Marker key={`landing-${l.id}`} position={getPosition(l)} icon={createEmojiIcon('🛸', l.landingCode)}>
-            <Popup>{l.locationName}</Popup>
+      {/* 👽 חייזרים */}
+      {aliens.map(a => (
+        <React.Fragment key={`alien-${a.id}`}>
+          <Marker position={[a.lat, a.lng]} icon={createEmojiIcon('👽', a.alienCode)}>
+            <Popup>{`Alien ${a.alienCode}`}</Popup>
           </Marker>
-        ))}
+          {a.route && a.route.length > 0 && (
+            <Polyline positions={a.route.map(p => [p[0], p[1]])} color="purple" />
+          )}
+        </React.Fragment>
+      ))}
 
-        {/* 👽 חייזרים */}
-        {aliens.map(a => (
-          <React.Fragment key={`alien-${a.id}`}>
-            <Marker position={getPosition(a)} icon={createEmojiIcon('👽', a.alienCode)}>
-              <Popup>{`Alien ${a.alienCode}`}</Popup>
-            </Marker>
-            {a.route && a.route.length > 0 && (
-              <Polyline positions={a.route.map(p => [p[0], p[1]])} color="purple" />
-            )}
-          </React.Fragment>
-        ))}
+      {/* 🚙 טקילות */}
+      {takilas.map(t => (
+        <Marker
+          key={`takila-${t.id}`}
+          position={[t.lat, t.lng]}
+          icon={createEmojiIcon('🚙', t.takilaCode, t.showFightersOut ? 'WAITING' : '')}
+        >
+          <Popup>{`Takila ${t.takilaCode}`}</Popup>
+        </Marker>
+      ))}
 
-        {/* 🚙 טקילות */}
-        {takilas.map(t => (
-          <Marker
-            key={`takila-${t.id}`}
-            position={getPosition(t)}
-            icon={createEmojiIcon('🚙', t.takilaCode, t.showFightersOut ? 'WAITING' : '')}
-          >
-            <Popup>{`Takila ${t.takilaCode}`}</Popup>
-          </Marker>
-        ))}
+      {/* 🧍 לוחמים */}
+      {fighters.map(f => (
+        <Marker key={`fighter-${f.id}`} position={[f.lat, f.lng]} icon={createEmojiIcon('🧍', f.phase)}>
+          <Popup>{`Fighter`}</Popup>
+        </Marker>
+      ))}
 
-        {/* 🧍 לוחמים */}
-        {fighters.map(f => (
-          <Marker
-            key={`fighter-${f.id}`}
-            position={getPosition(f)}
-            icon={createEmojiIcon('🧍', f.phase)}
-          >
-            <Popup>{`Fighter`}</Popup>
-          </Marker>
-        ))}
+      {/* 🔫 יריות */}
+      {shots.map((s, i) => (
+        <Polyline
+          key={`shot-${i}`}
+          positions={s.geometry.coordinates.map(c => [c[1], c[0]])}
+          pathOptions={{
+            color:
+              s.properties.shotType === 'fighter'
+                ? 'red'
+                : s.properties.shotType === 'alien'
+                ? 'blue'
+                : s.properties.shotType === 'takila'
+                ? 'purple'
+                : 'green',
+            weight: 3,
+          }}
+        />
+      ))}
 
-        {shots.map((s, i) => (
-          <Polyline
-            key={`shot-${i}`}
-            positions={s.geometry.coordinates.map(c => [c[1], c[0]])}
-            pathOptions={{
-              color:
-                s.properties.shotType === 'fighter'
-                  ? 'red'
-                  : s.properties.shotType === 'alien'
-                  ? 'blue'
-                  : s.properties.shotType === 'takila'
-                  ? 'purple'
-                  : 'green',
-              weight: 3,
-            }}
-          />
-        ))}
-
-        {/* 💥 פיצוצים */}
-        {explosions.map((e, i) => (
-          <Marker
-            key={`explosion-${i}`}
-            position={[e.lat, e.lng]}
-            icon={createEmojiIcon('💥')}
-          />
-        ))}
-      </MapContainer>
-    </>
+      {/* 💥 פיצוצים */}
+      {explosions.map((e, i) => (
+        <Marker key={`explosion-${i}`} position={[e.lat, e.lng]} icon={createEmojiIcon('💥')} />
+      ))}
+    </MapContainer>
   );
 }
